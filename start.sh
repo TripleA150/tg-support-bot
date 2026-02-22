88ad3aa5ae0affd0e67c7e49d7ab09f4f6a3b495
TripleA <46062533+TripleA150@users.noreply.github.com>
2026-02-22 11:27:25 +1000

    Update start.sh

--- start.sh
+++ start.sh
@@ -12,18 +12,18 @@ function run_step {
     fi
 }
 
-echo "🔄 Обновление списка пакетов и обновление системы..."
-#sudo apt update && sudo apt upgrade -y
+# echo "🔄 Обновление списка пакетов и обновление системы..."
+# sudo apt update && sudo apt upgrade -y
 
 # 🔹 Установка Certbot и плагина для Nginx (обязательный, но проверяем наличие)
-echo "🔧 Проверка и установка Certbot и плагина для Nginx..."
-if ! command -v certbot >/dev/null 2>&1; then
-    echo "certbot не найден, пытаемся установить..."
-    sudo apt install -y certbot python3-certbot-nginx || \
-        echo "⚠️ Не удалось полностью установить certbot через apt. Если certbot уже установлен, продолжаем."
-else
-    echo "certbot уже установлен, установка пропущена."
-fi
+# echo "🔧 Проверка и установка Certbot и плагина для Nginx..."
+# if ! command -v certbot >/dev/null 2>&1; then
+#     echo "certbot не найден, пытаемся установить..."
+#     sudo apt install -y certbot python3-certbot-nginx || \
+#         echo "⚠️ Не удалось полностью установить certbot через apt. Если certbot уже установлен, продолжаем."
+# else
+#     echo "certbot уже установлен, установка пропущена."
+# fi
 
 # Проверка наличия .env
 if [ ! -f .env ]; then
@@ -64,13 +64,13 @@ PROJECT_GROUP="www-data"
 run_step "sudo chown -R $PROJECT_USER:$PROJECT_GROUP ." "Назначение владельца $PROJECT_USER:$PROJECT_GROUP для всех файлов проекта"
 
 # Получение сертификатов
-PGADMIN_DOMAIN="pgadmin.$MAIN_DOMAIN"
-GRAFANA_DOMAIN="grafana.$MAIN_DOMAIN"
-NODE_DOMAIN="node.$MAIN_DOMAIN"
-run_step "sudo certbot certonly --standalone -d $MAIN_DOMAIN" "Выпуск сертификата для $MAIN_DOMAIN"
-run_step "sudo certbot certonly --standalone -d $PGADMIN_DOMAIN" "Выпуск сертификата для $PGADMIN_DOMAIN"
-run_step "sudo certbot certonly --standalone -d $GRAFANA_DOMAIN" "Выпуск сертификата для $GRAFANA_DOMAIN"
-run_step "sudo certbot certonly --standalone -d $NODE_DOMAIN" "Выпуск сертификата для $NODE_DOMAIN"
+# PGADMIN_DOMAIN="pgadmin.$MAIN_DOMAIN"
+# GRAFANA_DOMAIN="grafana.$MAIN_DOMAIN"
+# NODE_DOMAIN="node.$MAIN_DOMAIN"
+# run_step "sudo certbot certonly --standalone -d $MAIN_DOMAIN" "Выпуск сертификата для $MAIN_DOMAIN"
+# run_step "sudo certbot certonly --standalone -d $PGADMIN_DOMAIN" "Выпуск сертификата для $PGADMIN_DOMAIN"
+# run_step "sudo certbot certonly --standalone -d $GRAFANA_DOMAIN" "Выпуск сертификата для $GRAFANA_DOMAIN"
+# run_step "sudo certbot certonly --standalone -d $NODE_DOMAIN" "Выпуск сертификата для $NODE_DOMAIN"
 
 # Конфигурация Nginx
 run_step "sed 's|__MAIN_DOMAIN__|$MAIN_DOMAIN|g' docker/nginx/default.conf.template > docker/nginx/default.conf" "Создание конфигурации Nginx"
