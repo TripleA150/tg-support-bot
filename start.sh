b3e5f9e5d378ca66bccb3d6174efa012828558b3
TripleA <46062533+TripleA150@users.noreply.github.com>
2026-02-22 12:31:24 +1000

    Update start.sh

--- start.sh
+++ start.sh
@@ -12,19 +12,6 @@ function run_step {
     fi
 }
 
-# echo "🔄 Обновление списка пакетов и обновление системы..."
-# sudo apt update && sudo apt upgrade -y
-
-# 🔹 Установка Certbot и плагина для Nginx (обязательный, но проверяем наличие)
-# echo "🔧 Проверка и установка Certbot и плагина для Nginx..."
-# if ! command -v certbot >/dev/null 2>&1; then
-#     echo "certbot не найден, пытаемся установить..."
-#     sudo apt install -y certbot python3-certbot-nginx || \
-#         echo "⚠️ Не удалось полностью установить certbot через apt. Если certbot уже установлен, продолжаем."
-# else
-#     echo "certbot уже установлен, установка пропущена."
-# fi
-
 # Проверка наличия .env
 if [ ! -f .env ]; then
     echo "❌ Ошибка: .env файл не найден"
@@ -63,20 +50,11 @@ PROJECT_USER="www-data"
 PROJECT_GROUP="www-data"
 run_step "sudo chown -R $PROJECT_USER:$PROJECT_GROUP ." "Назначение владельца $PROJECT_USER:$PROJECT_GROUP для всех файлов проекта"
 
-# Получение сертификатов
-# PGADMIN_DOMAIN="pgadmin.$MAIN_DOMAIN"
-# GRAFANA_DOMAIN="grafana.$MAIN_DOMAIN"
-# NODE_DOMAIN="node.$MAIN_DOMAIN"
-# run_step "sudo certbot certonly --standalone -d $MAIN_DOMAIN" "Выпуск сертификата для $MAIN_DOMAIN"
-# run_step "sudo certbot certonly --standalone -d $PGADMIN_DOMAIN" "Выпуск сертификата для $PGADMIN_DOMAIN"
-# run_step "sudo certbot certonly --standalone -d $GRAFANA_DOMAIN" "Выпуск сертификата для $GRAFANA_DOMAIN"
-# run_step "sudo certbot certonly --standalone -d $NODE_DOMAIN" "Выпуск сертификата для $NODE_DOMAIN"
-
 # Конфигурация Nginx
 run_step "sed 's|__MAIN_DOMAIN__|$MAIN_DOMAIN|g' docker/nginx/default.conf.template > docker/nginx/default.conf" "Создание конфигурации Nginx"
 
 # Запуск Docker Compose
-run_step "docker-compose build" "Запуск Docker Compose"
+run_step "docker compose build" "Запуск Docker Compose"
 
 # 🔹 Запуск приложения (app) первым
 run_step "docker compose up -d app" "Запуск контейнера app"
@@ -97,11 +75,5 @@ run_step "docker compose exec app bash -c 'php artisan key:generate'" "Гене�
 # 🔹 Запуск остальных сервисов
 run_step "docker compose down" "Отключение сервисов"
 run_step "docker compose up -d" "Перезапуск сервисов"
-
-# Исправление прав для Grafana
-# echo "🔧 Проверка и исправление прав доступа к ./docker/grafana..."
-# docker compose logs grafana 2>&1 | grep -q "GF_PATHS_DATA='/var/lib/grafana' is not writable" && \
-# run_step "sudo chown -R 472:472 ./docker/grafana" "Исправление прав для Grafana (472:472)"
-# echo "✅ Права Grafana проверены и исправлены при необходимости"
 
 echo "✅ Скрипт выполнен успешно!"
